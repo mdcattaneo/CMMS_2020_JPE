851cd31da7906581e296c752ab33580ead315c34
Matias D. Cattaneo <cattaneo@princeton.edu>
2021-10-22 06:43:42 -0400

    .

--- Cattaneo-Ma-Masatlioglu-Suleymanov_2020_JPE.R
+++ Cattaneo-Ma-Masatlioglu-Suleymanov_2020_JPE.R
@@ -1,6 +1,6 @@
 ################################################################################
-# Replication file for Cattaneo, Ma, Masatlioglu and Suleymanov (2019)
-# 28-AUG-2019
+# Replication file for Cattaneo, Ma, Masatlioglu and Suleymanov (2020)
+# 20-OCT-2021
 ################################################################################
 
 # This replication file provides R code to test against 5 preferences for 1000 
@@ -17,40 +17,6 @@
 
 rm(list=ls(all=TRUE))
 library("ramchoice")
-
-################################################################################
-# Generate Data
-################################################################################
-
-# This function simulates dataset under the Logit attention rule
-#   n    : effective sample size
-#   uSize: number of alternatives
-#   mSize: size of menu/choice problem
-#   par  : parameter
-
-genData <- function(n, uSize, mSize, varsig) {
-  # determine population choice rule
-  prob_vec <- rep(0, mSize)
-  for (i in 1:mSize) { # enumerate over alternatives
-    for (j in 1:(mSize-i+1)) { # enumerate over consideration set size
-      temp1 <- factorial(mSize-i) / factorial(j-1) / factorial(mSize-i-j+1)
-      temp2 <- j^varsig / 
-        sum((1:mSize)^varsig * factorial(mSize) / factorial(1:mSize) / factorial((mSize-1):0))
-      prob_vec[i] = prob_vec[i] + temp1 * temp2
-    }
-  }
-  # initialize
-  allMenus <- t(combn(uSize, mSize))
-  menu <- choice <- matrix(0, nrow=n*nrow(allMenus), ncol=uSize) 
-  
-  for (i in 1:nrow(allMenus)) {
-    for (j in 1:n) {
-      menu[j+(i-1)*n, allMenus[i, ]] <- 1
-      choice[j+(i-1)*n, sort(allMenus[i, ])[rmultinom(1, 1, prob_vec) == 1]] <- 1
-    }
-  }
-  return(list(menu=menu, choice=choice))
-}
 
 ################################################################################
 # Prepare for simulation
@@ -77,9 +43,7 @@ result <- matrix(0, ncol=5, nrow=repe)
 # Inference
 ################################################################################
 
-# The whole exercise takes 8 minutes (0.32s for one Monte Carlo repetition). 
-# System: MacOS 10.13.1, R 3.4.1 
-#   2017 Macbook Pro 13inch, 2.3 GHz Intel Core i5, 16 GB 2133 MHz LPDDR3 
+# The whole exercise takes 10-20 minutes
 
 # Final result:
 #   --------------------------------
@@ -103,16 +67,17 @@ for (i in 1:repe) { # iterate through 2000 simulated datasets
   # Generate data
   menu <- choice <- matrix(0, nrow=0, ncol=5)
   for (j in 5:2) {
-    temp <- genData(n, 5, j, varsig)
+    temp <- logitSimu(n, 5, j, varsig)
     menu <- rbind(menu, temp$menu); choice <- rbind(choice, temp$choice)
   }
 
   # Inference result
-  temp_ram <- rAtte(menu, choice, pref_list = pref_list, method = "GMS",
-                    nCritSimu = 2000,
-                    BARatio2MS = 0.1, BARatio2UB = 0.1, MNRatioGMS = NULL,
-                    limDataCorr = FALSE,
-                    attBinary = 1)
+  temp_ram <- revealPref(menu, choice, pref_list = pref_list, method = "GMS",
+                         nCritSimu = 2000,
+                         BARatio2MS = 0.1, BARatio2UB = 0.1, MNRatioGMS = NULL,
+                         RAM = TRUE, AOM = FALSE,
+                         limDataCorr = FALSE,
+                         attBinary = 1)
 
   # Add inference result to the "result" matrix. We use the proposed critical value,
   #   and nominal level 0.05
